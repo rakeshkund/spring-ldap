e60e50e6bda118211f828008e19d1f1d1416f1c0
Rakesh Kundu <rkshknd@yahoo.co.in>
2019-03-23 22:13:42 +0530

    First commit ldap

---
+++ auth-ldap/src/test/java/org/spring/auth_ldap/SampleTest.java
@@ -0,0 +1,21 @@
+package org.spring.auth_ldap;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SampleTest {
+	
+	@Test
+	public void testOne(){
+		assertEquals(1,1);
+		
+	}
+	
+//	@Test
+	public void testTwo(){
+		assertEquals(1,2);
+		
+	}
+
+}
